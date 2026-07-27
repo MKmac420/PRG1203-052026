52325e9bc1f9acd61c329cbab1c820f83e8b229a
MKmac420 <65330895+MKmac420@users.noreply.github.com>
2026-07-27 20:33:43 +0800

    Create Input.java

---
+++ src/Input.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+/* TODO
+a more proper input detection system
+*/
+
+public class Input {
+    private Scanner player_input = new Scanner(System.in);
+
+
+    public int readInt() {
+        while (true) {
+            try {
+                return Integer.parseInt(player_input.nextLine()); // return a valid value only
+            } catch (NumberFormatException e) {
+                System.out.println("Invalid input! Please enter a number.");
+            }
+        }
+    }
+
+    public String readString() {
+        String input;
+        while (true) {
+        input = player_input.nextLine().trim(); // remove whitespaces if any
+        if (!input.isEmpty()) { // no empty input
+            input = input.substring(0, Math.min(input.length(), 20)); // truncates to only 20 chars
+            return input;
+        }
+            System.out.println("Input cannot be empty! Please enter something!");
+
+        }
+    }
+
+    public void promptEnter() {
+        player_input.nextLine();
+    }
+}
