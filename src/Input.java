f3116df418ec91bd1beb4f5b6e00ecfe8f74e500
MKmac420 <65330895+MKmac420@users.noreply.github.com>
2026-07-31 18:30:37 +0800

    Update Input.java

--- src/Input.java
+++ src/Input.java
@@ -11,9 +11,22 @@ public class Input {
     public int readInt() {
         while (true) {
             try {
-                return Integer.parseInt(player_input.nextLine()); // return a valid value only
+                return Integer.parseInt(player_input.nextLine().trim()); // return a valid value only
             } catch (NumberFormatException e) {
                 System.out.println("Invalid input! Please enter a number.");
+            }
+        }
+    }
+
+    public int readInt(int maximum) { // overloaded method to restrict max value
+        while (true) {
+            int selection = readInt();
+
+            if (selection > 0 && selection <= maximum) { // selection can only be between 1 and max
+                return selection;
+            }
+            else {
+                System.out.printf("Invalid input! Please pick between 1 to %d only", maximum);
             }
         }
     }
@@ -21,11 +34,11 @@ public class Input {
     public String readString() {
         String input;
         while (true) {
-        input = player_input.nextLine().trim(); // remove whitespaces if any
-        if (!input.isEmpty()) { // no empty input
-            input = input.substring(0, Math.min(input.length(), 20)); // truncates to only 20 chars
-            return input;
-        }
+            input = player_input.nextLine().trim(); // remove whitespaces if any
+            if (!input.isEmpty()) { // no empty input
+                input = input.substring(0, Math.min(input.length(), 20)); // truncates to only 20 chars
+                return input;
+            }
             System.out.println("Input cannot be empty! Please enter something!");
 
         }
