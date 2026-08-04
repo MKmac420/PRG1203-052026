d1f6271c0f04ba5f4a14bc0c84b88ad5f1bb0cc1
yutot777 <yutot777@gmail.com>
2026-08-05 05:25:21 +0800

    implement dealer class

--- src/Dealer.java
+++ src/Dealer.java
@@ -1,5 +1,53 @@
-public class Dealer {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+public class Dealer extends Participant {
+// constructor
+    public Dealer(String name) {
+        super(name);
+    }
+
+    @Override
+    public boolean isDealer() {
+    	// is a dealer, return true
+        return true;
+    }
+
+    @Override
+    public void playTurn(Deck deck, HouseRules rules) {
+    	// flip the hidden card
+        for (Card card : getHand().getCards()) {
+            card.setHidden(false);
+        }
+
+        System.out.printf("\n--- %s's Turn ---\n", getName());
+        // Loop until the hand is busted
+        while (!getHand().isBusted()) {
+            int score = getHand().getScore();
+
+            // if over 17, stop the loop
+            if (score > 17) {
+                stand();
+                System.out.printf("%s stands with score %d.\n", getName(), score);
+                break;
+            } else if (score == 17) {
+                // If the soft 17 rule is enabled, hit. if not stand.
+                if (getHand().isSoft() && rules.isHitSoft17()) {
+                    System.out.printf("%s hits.\n", getName());
+                    hit(deck);
+                } else {
+                    stand();
+                    System.out.printf("%s stands with score 17.\n", getName());
+                    break;
+                }
+            } else {
+            	// if less than 16, hit.
+                System.out.printf("%s hits.\n", getName());
+                hit(deck);
+            }
+
+            // Checking if busted
+            if (getHand().isBusted()) {
+                System.out.printf("%s busted with score %d!\n", getName(), getHand().getScore());
+                break;
+            }
+        }
     }
 }
