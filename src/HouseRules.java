449984ee0d9d19067a486fc61375fb9afbae5751
jameelchuupa7 <jameelchuupa@gmail.com>
2026-08-05 23:36:15 +0800

    Update HouseRules.java

--- src/HouseRules.java
+++ src/HouseRules.java
@@ -1,5 +1,27 @@
 public class HouseRules {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+    private int numberOfDecks;
+    private boolean hitSoft17;
+
+    // Constructor 
+    public HouseRules() {
+        this.numberOfDecks = 1;
+        this.hitSoft17 = true;
+    }
+
+
+    public HouseRules(int numberOfDecks, boolean hitSoft17) {
+        this.numberOfDecks = numberOfDecks;
+        this.hitSoft17 = hitSoft17;
+    }
+
+    // Getter for number of decks
+    public int getNumberOfDecks() {
+        return numberOfDecks;
+    }
+
+    // Getter for Soft 17 rule
+    public boolean isHitSoft17() {
+        return hitSoft17;
     }
 }
