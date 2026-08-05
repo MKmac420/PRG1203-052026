c358b5d2ea0d221c251b5c5095b412747da13480
Tomato◇♪ <dingtzejian@gmail.com>
2026-08-06 02:08:34 +0800

    Update Hand.java

--- src/Hand.java
+++ src/Hand.java
@@ -1,5 +1,67 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Hand {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
+	private List<Card> cards;
+
+	public Hand() {
+		cards = new ArrayList<>();
+	}
+
+	public void addCard(Card card) {
+		cards.add(card);
+	}
+
+	public void clear() {
+		cards.clear();
+	}
+
+	public List<Card> getCards() {
+		return cards;
+	}
+
+	public int getScore() {
+		int score = 0;
+		int aceCount = 0;
+
+		for (Card card : cards) {
+			score += card.getValue();
+
+			if (card.getRank().equals("Ace")) {
+				aceCount++;
+			}
+		}
+
+		// Convert Aces from 11 to 1 if necessary
+		while (score > 21 && aceCount > 0) {
+			score -= 10;
+			aceCount--;
+		}
+
+		return score;
+	}
+
+	public boolean isSoft() {
+		int score = 0;
+		int aceCount = 0;
+
+		for (Card card : cards) {
+			score += card.getValue();
+
+			if (card.getRank().equals("Ace")) {
+				aceCount++;
+			}
+		}
+
+		while (score > 21 && aceCount > 0) {
+			score -= 10;
+			aceCount--;
+		}
+
+		return aceCount > 0;
+	}
+
+	public boolean isBusted() {
+		return getScore() > 21;
+	}
 }
